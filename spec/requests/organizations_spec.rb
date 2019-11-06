7623778da174edefa08350eb0b097fc1a59ec3df
Sebastián Salata Ruiz-Tagle <sebastian@salatart.com>
2019-11-05 21:48:38 -0300

    test(organizations): refactor routes tests to use behaves-like syntax (#21)

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -2,45 +2,31 @@
 
 RSpec.describe 'Organizations requests' do
   describe 'GET /organizations' do
+    let(:http_method) { :get }
+    let(:url) { '/organizations' }
     total = 30
-    url = '/organizations'
 
     before do
       total.times { create(:organization) }
     end
 
-    context 'when the request is not authenticated' do
-      it 'responds with an unauthorized status' do
-        get url
-        expect(response).to have_http_status(:unauthorized)
-      end
-    end
+    it_behaves_like 'a request that needs authentication'
 
     context 'when the user is authenticated' do
       %i[org_admin employee].each do |role_name|
         context "when the user is an #{role_name}" do
-          it 'responds with a forbidden status' do
-            user = create(role_name)
-            get url, headers: authenticated_header(user)
-            expect(response).to have_http_status(:forbidden)
-          end
+          let(:requester) { create(role_name) }
+          it_behaves_like 'a forbidden request'
         end
       end
 
       context 'when the user is an admin' do
-        let(:admin) { create(:admin) }
+        let(:requester) { create(:admin) }
 
-        before { get url, headers: authenticated_header(admin) }
+        before { get url, headers: authenticated_header(requester) }
 
-        it 'responds with an ok status' do
-          expect(response).to have_http_status(:ok)
-        end
-
-        it 'responds with pagination headers' do
-          expect(response.headers['x-page']).to eql('1')
-          expect(response.headers['x-per-page']).to eql('25')
-          expect(response.headers['x-total']).to eql(total.to_s)
-        end
+        it_behaves_like 'an ok request'
+        it_behaves_like 'a paginated request'
 
         it 'responds with the serialized organizations' do
           expected = Organization.all.limit(25).map do |organization|
@@ -53,183 +39,142 @@ RSpec.describe 'Organizations requests' do
   end
 
   describe 'GET /organizations/:id' do
-    let(:organization) { create(:organization) }
+    let(:http_method) { :get }
     let(:url) { "/organizations/#{organization.id}" }
+    let(:organization) { create(:organization) }
+    let(:another_organization) { create(:organization) }
 
-    context 'when the request is not authenticated' do
-      it 'responds with an unauthorized status' do
-        get url
-        expect(response).to have_http_status(:unauthorized)
+    shared_examples_for 'a successful show request' do
+      before { get url, headers: authenticated_header(requester) }
+
+      it_behaves_like 'an ok request'
+
+      it 'responds with the serialized organization' do
+        expect(response.body).to eql(OrganizationSerializer.new(organization).to_json)
       end
     end
+
+    it_behaves_like 'a request that needs authentication'
 
     context 'when the user is authenticated' do
       %i[org_admin employee].each do |role_name|
         context "when the user is an #{role_name}" do
           context 'when the user is not from that organization' do
-            it 'responds with a forbidden status' do
-              user = create(role_name)
-              get url, headers: authenticated_header(user)
-              expect(response).to have_http_status(:forbidden)
-            end
+            let(:requester) { create(role_name, organization: another_organization) }
+            it_behaves_like 'a forbidden request'
           end
 
           context 'when the user is from that organization' do
-            let(:user) { create(role_name, organization_id: organization.id) }
-
-            before { get url, headers: authenticated_header(user) }
-
-            it 'responds with an ok status' do
-              expect(response).to have_http_status(:ok)
-            end
-
-            it 'responds with the serialized organization' do
-              expect(response.body).to eql(OrganizationSerializer.new(organization).to_json)
-            end
+            let(:requester) { create(role_name, organization: organization) }
+            it_behaves_like 'a successful show request'
           end
         end
       end
 
       context 'when the user is an admin' do
-        let(:admin) { create(:admin) }
-
-        before { get url, headers: authenticated_header(admin) }
-
-        it 'responds with an ok status' do
-          expect(response).to have_http_status(:ok)
-        end
-
-        it 'responds with the serialized organization' do
-          expect(response.body).to eql(OrganizationSerializer.new(organization).to_json)
-        end
+        let(:requester) { create(:admin) }
+        it_behaves_like 'a successful show request'
       end
     end
   end
 
   describe 'POST /organizations' do
-    def create_organization(requesting_user = nil)
-      headers = requesting_user ? authenticated_header(requesting_user) : {}
-      post '/organizations', headers: headers, params: { name: 'created-org-name' }
-    end
+    let(:http_method) { :post }
+    let(:url) { '/organizations' }
+    valid_body = { name: 'created-org-name' }
+    let(:body) { valid_body }
 
-    context 'when the request is not authenticated' do
-      it 'responds with an unauthorized status' do
-        create_organization
-        expect(response).to have_http_status(:unauthorized)
-      end
-    end
+    it_behaves_like 'a request that needs authentication'
 
     context 'when the user is authenticated' do
       %i[org_admin employee].each do |role_name|
         context "when the user is an #{role_name}" do
-          it 'responds with a forbidden status' do
-            user = create(role_name)
-            create_organization(user)
-            expect(response).to have_http_status(:forbidden)
-          end
+          let(:requester) { create(role_name) }
+          it_behaves_like 'a forbidden request'
         end
       end
 
       context 'when the user is an admin' do
-        let(:admin) { create(:admin) }
+        let(:requester) { create(:admin) }
 
-        before { create_organization(admin) }
-
-        it 'responds with a created status' do
-          expect(response).to have_http_status(:created)
+        context 'when no name is sent' do
+          let(:body) { { name: nil } }
+          it_behaves_like 'an unprocessable_entity request'
         end
 
-        it 'responds with the serialized created organization' do
-          id = ActiveSupport::JSON.decode(response.body)['id']
-          organization = Organization.find(id)
-          expect(response.body).to eql(OrganizationSerializer.new(organization).to_json)
+        context 'when a valid new name is sent' do
+          before { post url, headers: authenticated_header(requester), params: body }
+
+          it_behaves_like 'a created request'
+
+          it 'responds with the serialized created organization' do
+            id = ActiveSupport::JSON.decode(response.body)['id']
+            organization = Organization.find(id)
+            expect(response.body).to eql(OrganizationSerializer.new(organization).to_json)
+          end
         end
       end
     end
   end
 
   describe 'PUT /organizations/:id' do
-    let(:organization) { create(:organization, name: 'org-name') }
+    let(:http_method) { :put }
+    let(:url) { "/organizations/#{organization.id}" }
+    valid_body = { name: 'new-org-name' }
+    let(:body) { valid_body }
+    let(:organization) { create(:organization) }
 
-    def update_organization(name, requesting_user = nil)
-      headers = requesting_user ? authenticated_header(requesting_user) : {}
-      put "/organizations/#{organization.id}", headers: headers, params: { name: name }
-    end
-
-    context 'when the request is not authenticated' do
-      it 'responds with an unauthorized status' do
-        update_organization('name-a')
-        expect(response).to have_http_status(:unauthorized)
-      end
-    end
+    it_behaves_like 'a request that needs authentication'
 
     context 'when the user is authenticated' do
       %i[org_admin employee].each do |role_name|
         context "when the user is an #{role_name}" do
-          it 'responds with a forbidden status' do
-            user = create(role_name, organization: organization)
-            update_organization('name-b', user)
-            expect(response).to have_http_status(:forbidden)
-          end
+          let(:requester) { create(role_name, organization: organization) }
+          it_behaves_like 'a forbidden request'
         end
       end
 
       context 'when the user is an admin' do
-        let(:admin) { create(:admin) }
-        let(:updated_organization) { Organization.find(organization.id) }
-        name = 'name-from-admin'
+        let(:requester) { create(:admin) }
 
-        before { update_organization(name, admin) }
-
-        it 'responds with an ok status' do
-          expect(response).to have_http_status(:ok)
+        before do
+          put url, headers: authenticated_header(requester), params: body
+          @updated = Organization.find(organization.id)
         end
 
+        it_behaves_like 'an ok request'
+
         it 'responds with the serialized updated organization' do
-          expect(response.body).to eql(OrganizationSerializer.new(updated_organization).to_json)
+          expect(response.body).to eql(OrganizationSerializer.new(@updated).to_json)
         end
 
         it 'udpates the name' do
-          expect(updated_organization.name).to eql(name)
+          expect(@updated.name).to eql(valid_body[:name])
         end
       end
     end
   end
 
   describe 'DELETE /organizations/:id' do
+    let(:http_method) { :delete }
+    let(:url) { "/organizations/#{organization.id}" }
     let(:organization) { create(:organization) }
 
-    def destroy_organization(requesting_user = nil)
-      headers = requesting_user ? authenticated_header(requesting_user) : {}
-      delete "/organizations/#{organization.id}", headers: headers
-    end
-
-    context 'when the request is not authenticated' do
-      it 'responds with an unauthorized status' do
-        destroy_organization
-        expect(response).to have_http_status(:unauthorized)
-      end
-    end
+    it_behaves_like 'a request that needs authentication'
 
     context 'when the user is authenticated' do
       %i[org_admin employee].each do |role_name|
         context "when the user is an #{role_name}" do
-          it 'responds with a forbidden status' do
-            user = create(role_name, organization: organization)
-            destroy_organization(user)
-            expect(response).to have_http_status(:forbidden)
-          end
+          let(:requester) { create(role_name, organization: organization) }
+          it_behaves_like 'a forbidden request'
         end
       end
 
       context 'when the user is an admin' do
-        let(:admin) { create(:admin) }
+        let(:requester) { create(:admin) }
+        before { delete url, headers: authenticated_header(requester) }
 
-        before { destroy_organization(admin) }
-
-        it 'responds with a no_content status' do
-          expect(response).to have_http_status(:no_content)
-        end
+        it_behaves_like 'a no_content request'
 
         it 'destroys the organization' do
           expect(Organization.find_by(id: organization.id)).to be nil
