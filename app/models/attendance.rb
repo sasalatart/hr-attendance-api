3020f86f6c8863ab4dac678f47670bdeb7298c0f
Sebastián Salata R-T <sa.salatart@gmail.com>
2019-11-08 17:38:03 -0300

    refactor(attendances): improve no_overlapping and no_other_in_same_day validations

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -30,19 +30,19 @@ class Attendance < ApplicationRecord
 
   private
 
-  def overlapping_count
+  def overlapping_ids
     Attendance.where(employee_id: employee_id).where(
       'entered_at >= ? and entered_at <= ?', entered_at, left_at
     ).or(Attendance.where(
            'left_at >= ? and left_at <= ?', entered_at, left_at
          )).or(Attendance.where(
                  'entered_at <= ? and left_at >= ?', entered_at, left_at
-               )).count
+               )).pluck(:id)
   end
 
   def no_overlapping
-    return if new_record? && overlapping_count.zero?
-    return if persisted? && overlapping_count == 1
+    ids = overlapping_ids
+    return if ids.empty? || (ids.size == 1 && ids.include?(id))
 
     errors.add(:base, :no_overlapping)
   end
@@ -54,28 +54,26 @@ class Attendance < ApplicationRecord
     errors.add(:employee_id, :only_for_employees)
   end
 
-  def existing_daily_count_for(attribute_name)
+  def no_other_in_same_day?(attribute_name)
     value = self[attribute_name]
     bod = value.beginning_of_day
     eod = value.end_of_day
-    Attendance.where(employee_id: employee_id).where(
+    ids = Attendance.where(employee_id: employee_id).where(
       "#{attribute_name} >= ? and #{attribute_name} <= ?", bod, eod
-    ).count
+    ).pluck(:id)
+    ids.empty? || ids.size == 1 && ids.include?(id)
   end
 
   def only_one_left_at_per_employee_per_day
     return unless left_at && will_save_change_to_attribute?(:left_at)
-    return if existing_daily_count_for(:left_at).zero?
+    return if no_other_in_same_day?(:left_at)
 
     errors.add(:left_at, :only_one_per_employee_per_day)
   end
 
   def only_one_entered_at_per_employee_per_day
     return unless will_save_change_to_attribute?(:entered_at)
-
-    existing_count = existing_daily_count_for(:entered_at)
-    return if existing_count.zero? && new_record?
-    return if existing_count == 1 && persisted?
+    return if no_other_in_same_day?(:entered_at)
 
     errors.add(:entered_at, :only_one_per_employee_per_day)
   end
