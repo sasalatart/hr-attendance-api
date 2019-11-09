03fbe0c40fc13165b5b6e7a799c9fffd50b81fc2
Sebastián Salata Ruiz-Tagle <sebastian@salatart.com>
2019-11-09 18:17:20 -0300

    feat(seeds): add attendances (#38)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 DEFAULT_PASSWORD = 'napoleon'
-total_organizations = 35
+total_organizations = 5
 org_admins_per_organization = 5
 employees_per_organization = 35
 
@@ -11,6 +11,18 @@ def common_user_params
     surname: Faker::Name.last_name,
     password: DEFAULT_PASSWORD
   }
+end
+
+def add_attendances_to(employee)
+  30.times do |index|
+    bod = (index + 1).days.ago.beginning_of_day
+
+    next if bod.saturday? || bod.sunday?
+
+    entered_at = bod + 9.hours + (rand * 30).minutes
+    left_at = entered_at + 9.hours + (rand * 30).minutes
+    employee.attendances.create!(entered_at: entered_at, left_at: left_at)
+  end
 end
 
 User.create!({ role: :admin,
@@ -26,14 +38,17 @@ total_organizations.times do |org_idx|
   organization = Organization.create!(name: name)
 
   org_admins_per_organization.times do |org_admin_idx|
-    User.create!({ role: :org_admin,
-                   organization: organization,
-                   email: "#{org_idx}-#{org_admin_idx}-org-admin@example.org" }.merge!(common_user_params))
+    params = { role: :org_admin,
+               organization: organization,
+               email: "#{org_idx}-#{org_admin_idx}-org-admin@example.org" }
+    User.create!(params.merge!(common_user_params))
   end
 
   employees_per_organization.times do |employee_idx|
-    User.create!({ role: :employee,
-                   organization: organization,
-                   email: "#{org_idx}-#{employee_idx}-employee@example.org" }.merge!(common_user_params))
+    params = { role: :employee,
+               organization: organization,
+               email: "#{org_idx}-#{employee_idx}-employee@example.org" }
+    employee = User.create!(params.merge!(common_user_params))
+    add_attendances_to(employee)
   end
 end
