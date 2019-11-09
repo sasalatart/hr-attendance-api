0d7f30527b2f7c717a68132de4b4ec29076b21ae
Sebastián Salata Ruiz-Tagle <sebastian@salatart.com>
2019-11-09 19:48:11 -0300

    feat(attendances): serialize employees' fullnames (#39)

--- app/serializers/attendance_serializer.rb
+++ app/serializers/attendance_serializer.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class AttendanceSerializer < ActiveModel::Serializer
-  attributes :id, :employee_id, :entered_at, :left_at, :updated_at, :created_at
+  attributes :id, :employee_id, :employee_fullname, :entered_at, :left_at, :updated_at, :created_at
+
+  def employee_fullname
+    employee = object.employee
+    %i[name surname second_surname].map { |attribute| employee[attribute] }.compact.join(' ')
+  end
 end
